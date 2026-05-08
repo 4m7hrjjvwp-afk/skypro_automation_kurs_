89b3246c6dd25709b0ef4d8ab129104b0e33fe6f
4m7hrjjvwp-afk <agraevskaya@gmail.com>
2026-05-08 08:51:44 +0300

    домашнее задание 6 внесла правки в скрипт (ожидание появления всех картинок)

--- 06_lesson/wait_picture.py
+++ 06_lesson/wait_picture.py
@@ -6,14 +6,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager().install()))
-waiter = WebDriverWait(driver, 40)
+driver.maximize_window()
+waiter = WebDriverWait(driver, 10)
 
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
-
-waiter.until(
-    EC.visibility_of_element_located((By.CSS_SELECTOR, "#landscape"))
-)
+waiter.until(EC.presence_of_element_located((By.ID, "landscape")))
 
 search_src = driver.find_element(By.CSS_SELECTOR, "#award").get_attribute("src")
 
